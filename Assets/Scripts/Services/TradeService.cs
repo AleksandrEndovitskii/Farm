7993f94c3c3a921faac3d0a606c0ba387094b894
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-27 22:54:04 +0300

    added demo implementations of abilities to buy and sell items to TradeService

--- Assets/Scripts/Services/TradeService.cs
+++ Assets/Scripts/Services/TradeService.cs
@@ -1,7 +1,18 @@
-﻿namespace Services
+﻿using GameObjects.Production;
+using GameObjects.Utils;
+
+namespace Services
 {
     public class TradeService
     {
+        public void Sell(IProduction iProduction)
+        {
 
+        }
+
+        public void Buy(IBuyable buyable)
+        {
+
+        }
     }
 }
