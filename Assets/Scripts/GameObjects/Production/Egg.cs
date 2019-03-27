9b61ebd03af5836e341fe0745f57e8c608a3440d
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-27 22:27:52 +0300

    added Production items

---
+++ Assets/Scripts/GameObjects/Production/Egg.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GameObjects.Production
+{
+    /*
+        • Яйца можно продать, получив прибыль.
+     */
+    public class Egg : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/GameObjects/Production/Milk.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GameObjects.Production
+{
+    /*
+        • молоко можно продать, получив прибыль.
+     */
+    public class Milk : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
