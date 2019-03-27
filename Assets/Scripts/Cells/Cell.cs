77d1ae8e18b439d4eb0336109a0e17d2a29b7dd5
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-27 22:05:00 +0300

    added Cell

---
+++ Assets/Scripts/Cells/Cell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Cells
+{
+    public class Cell : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
