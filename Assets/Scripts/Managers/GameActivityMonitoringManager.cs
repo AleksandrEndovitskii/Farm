8fe58bde7523814f348529376d83e8e802a2e83c
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-03-28 03:52:38 +0300

    added GameActivityMonitoringManager

---
+++ Assets/Scripts/Managers/GameActivityMonitoringManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Services
+{
+    /*
+        • Автоматическое сохранение прогресса и загрузка игры с момента последнего сохранения.
+    */
+
+    public class GameActivityMonitoringManager : MonoBehaviour
+    {
+
+    }
+}
