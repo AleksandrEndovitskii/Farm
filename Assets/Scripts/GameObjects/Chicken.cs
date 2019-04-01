99ac485e208a95828717ee850ac929d07c1a7733
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-02 01:25:12 +0300

    added declaration of Chicken as IPlaceable

--- Assets/Scripts/GameObjects/Chicken.cs
+++ Assets/Scripts/GameObjects/Chicken.cs
@@ -6,7 +6,7 @@ namespace GameObjects
         • Если еды достаточно, то курица несёт одно яйцо за 10 сек
         • 1 единицы пшеницы хватает на 30 сек курице
      */
-    public class Chicken : IBuyable
+    public class Chicken : IBuyable, IPlaceable
     {
         public int BuyPrice { get; }
     }
