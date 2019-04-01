7e97f1aee1f4d4eec63ba2b9dfe6594bed81676c
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-02 01:12:07 +0300

    added ability to set price to IBuyable item

--- Assets/Scripts/Services/PriceService.cs
+++ Assets/Scripts/Services/PriceService.cs
@@ -1,12 +1,35 @@
-﻿using Utils;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameObjects.Utils;
+using Utils;
 
 namespace Services
 {
     public class PriceService : IInitializable
     {
+        private Dictionary<Type, int> _buyablePrices = new Dictionary<Type, int>();
+
         public void Initialize()
         {
-            //
+
+        }
+
+        public void SetPriceForType<T>(int price) where T : IBuyable
+        {
+            _buyablePrices.Add(typeof(T),price);
+        }
+
+        public int GetPriceForType<T>() where T : IBuyable
+        {
+            if (_buyablePrices.All(x => x.Key.GetType() != typeof(T)))
+            {
+                throw new ArgumentOutOfRangeException(string.Format("No price was specified for {0}", typeof(T).Name));
+            }
+
+            var keyValuePair = _buyablePrices.First(x => x.Key.GetType() == typeof(T));
+
+            return keyValuePair.Value;
         }
     }
 }
