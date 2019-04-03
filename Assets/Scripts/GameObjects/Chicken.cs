08d55ab8489b9d759b7b17ee61032feee7c00285
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-04 02:17:29 +0300

    added declaration of Cow and Chicken as IFuelRequiringProducer

--- Assets/Scripts/GameObjects/Chicken.cs
+++ Assets/Scripts/GameObjects/Chicken.cs
@@ -1,4 +1,4 @@
-﻿using GameObjects.Utils;
+using GameObjects.Utils;
 
 namespace GameObjects
 {
@@ -6,9 +6,18 @@ namespace GameObjects
         • Если еды достаточно, то курица несёт одно яйцо за 10 сек
         • 1 единицы пшеницы хватает на 30 сек курице
      */
-    public class Chicken : IBuyable, IPlaceable, IFeedable
+    public class Chicken : IBuyable, IPlaceable, IFeedable, IFuelRequiringProducer
     {
         public int BuyPrice { get; }
+
+        public int HaveFuelForSecondsCount { get; }
+        public int WillProduceAfterSecondsCount { get; }
+
+        public Chicken(int haveFuelForSecondsCount, int willProduceAfterSecondsCount)
+        {
+            HaveFuelForSecondsCount = haveFuelForSecondsCount;
+            WillProduceAfterSecondsCount = willProduceAfterSecondsCount;
+        }
 
         public void Feed(IFood food)
         {
--- Assets/Scripts/GameObjects/Cow.cs
+++ Assets/Scripts/GameObjects/Cow.cs
@@ -1,4 +1,4 @@
-﻿using GameObjects.Utils;
+using GameObjects.Utils;
 
 namespace GameObjects
 {
@@ -6,9 +6,18 @@ namespace GameObjects
         • Если еды достаточно, то корова даёт молоко раз в 20 сек;
         • 1 единицы пшеницы хватает на 20 сек корове;
      */
-    public class Cow : IBuyable, IPlaceable, IFeedable
+    public class Cow : IBuyable, IPlaceable, IFeedable, IFuelRequiringProducer
     {
         public int BuyPrice { get; }
+
+        public int HaveFuelForSecondsCount { get; }
+        public int WillProduceAfterSecondsCount { get; }
+
+        public Cow(int haveFuelForSecondsCount, int willProduceAfterSecondsCount)
+        {
+            HaveFuelForSecondsCount = haveFuelForSecondsCount;
+            WillProduceAfterSecondsCount = willProduceAfterSecondsCount;
+        }
 
         public void Feed(IFood food)
         {
