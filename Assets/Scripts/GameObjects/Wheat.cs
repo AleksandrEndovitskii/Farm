a08d1f112b0554f57c69dbe840b4b8dae4f70fbe
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-04 01:36:05 +0300

    added declaration of Wheat as IFood

--- Assets/Scripts/GameObjects/Wheat.cs
+++ Assets/Scripts/GameObjects/Wheat.cs
@@ -6,7 +6,7 @@ namespace GameObjects
         • Пшеница вырастает за 10 сек, после чего можно собрать урожай (1 единица урожая с одной клетки), затем рост начинается заново;
         • Пшеницей можно покормить курицу или корову;
      */
-    public class Wheat : IBuyable, IPlaceable
+    public class Wheat : IBuyable, IPlaceable, IFood
     {
         public int BuyPrice { get; }
     }
