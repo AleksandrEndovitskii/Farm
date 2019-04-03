3863888ca24975032298765d61169408355597c1
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-04 02:19:00 +0300

    added declaration of Cow and Chicken as IProducer

--- Assets/Scripts/GameObjects/Wheat.cs
+++ Assets/Scripts/GameObjects/Wheat.cs
@@ -1,4 +1,4 @@
-﻿using GameObjects.Utils;
+using GameObjects.Utils;
 
 namespace GameObjects
 {
@@ -6,8 +6,15 @@ namespace GameObjects
         • Пшеница вырастает за 10 сек, после чего можно собрать урожай (1 единица урожая с одной клетки), затем рост начинается заново;
         • Пшеницей можно покормить курицу или корову;
      */
-    public class Wheat : IBuyable, IPlaceable, IFood
+    public class Wheat : IBuyable, IPlaceable, IFood, IProducer
     {
         public int BuyPrice { get; }
+
+        public int WillProduceAfterSecondsCount { get; }
+
+        public Wheat(int willProduceAfterSecondsCount)
+        {
+            WillProduceAfterSecondsCount = willProduceAfterSecondsCount;
+        }
     }
 }
