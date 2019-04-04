cc07ff840d7d7c671a303e9a0200104379be4904
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-05 01:14:00 +0300

    added SellPriceDictionaryService

--- Assets/Scripts/Services/ProductionDurationDictionaryService.cs
+++ Assets/Scripts/Services/ProductionDurationDictionaryService.cs
@@ -9,17 +9,17 @@ namespace Services
     {
         public void Initialize()
         {
-            SetDurationForProduction<Wheat>(10); // Пшеница вырастает за 10 сек, после чего можно собрать урожай(1 единица урожая с одной клетки), затем рост начинается заново;
-            SetDurationForProduction<Chicken>(10); // Если еды достаточно, то курица несёт одно яйцо за 10 сек
-            SetDurationForProduction<Cow>(20); // Если еды достаточно, то корова даёт молоко раз в 20 сек;
+            SetProductionDurationForProducer<Wheat>(10); // Пшеница вырастает за 10 сек, после чего можно собрать урожай(1 единица урожая с одной клетки), затем рост начинается заново;
+            SetProductionDurationForProducer<Chicken>(10); // Если еды достаточно, то курица несёт одно яйцо за 10 сек
+            SetProductionDurationForProducer<Cow>(20); // Если еды достаточно, то корова даёт молоко раз в 20 сек;
         }
 
-        public void SetDurationForProduction<T>(int value) where T : IProducer
+        public void SetProductionDurationForProducer<T>(int value) where T : IProducer
         {
             base.SetValueForType<T>(value);
         }
 
-        public int GetDurationForProduction<T>() where T : IProducer
+        public int GetProductionDurationForProducer<T>() where T : IProducer
         {
             return base.GetValueForType<T>();
         }
---
+++ Assets/Scripts/Services/SellPriceDictionaryService.cs
@@ -0,0 +1,24 @@
+﻿using GameObjects.Utils;
+using Services.Utils;
+using Utils;
+
+namespace Services
+{
+    public class SellPriceDictionaryService : TypeValueDictionary, IInitializable
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void SetSellPriceForType<T>(int price) where T : ISellable
+        {
+            base.SetValueForType<T>(price);
+        }
+
+        public int GetSellPriceForType<T>() where T : ISellable
+        {
+            return base.GetValueForType<T>();
+        }
+    }
+}
