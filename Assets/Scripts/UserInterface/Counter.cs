6e69811653c87525bb4f454788f219038e15aac0
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-05 12:03:28 +0300

    added Counter
    123

---
+++ Assets/Scripts/UserInterface/Counter.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using Utils;
+
+namespace UserInterface
+{
+    public class Counter : MonoBehaviour, IInitializable
+    {
+        [SerializeField]
+        public TextMeshProUGUI captionTextMeshProText;
+        [SerializeField]
+        public TextMeshProUGUI valueTextMeshProText;
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
