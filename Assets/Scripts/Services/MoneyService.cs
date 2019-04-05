cab61e0139731276db8e8e626fd2ea31490f239d
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-05 12:04:51 +0300

    added event notification about money changing

--- Assets/Scripts/Services/MoneyService.cs
+++ Assets/Scripts/Services/MoneyService.cs
@@ -1,10 +1,31 @@
-﻿using Utils;
+﻿using System;
+using Utils;
 
 namespace Services
 {
     public class MoneyService : IInitializable
     {
-        public int MoneyAmount { get; set;}
+        public Action<int> MoneyAmountChanged = delegate {  };
+
+        private int _moneyAmount;
+        public int MoneyAmount
+        {
+            get
+            {
+                return _moneyAmount;
+            }
+            set
+            {
+                if (_moneyAmount == value)
+                {
+                    return;
+                }
+
+                _moneyAmount = value;
+
+                MoneyAmountChanged.Invoke(_moneyAmount);
+            }
+        }
 
         public void Initialize()
         {
