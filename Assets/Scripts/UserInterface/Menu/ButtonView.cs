50a5490e4266a2fd0fd39c9d8254bc31eb4986b3
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-04-08 05:12:49 +0300

    added button

---
+++ Assets/Scripts/UserInterface/Menu/ButtonView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface.Menu
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonView : MonoBehaviour
+    {
+        public Action<ButtonView> Clicked = delegate { };
+
+        [SerializeField]
+        private Image _contentImage;
+
+        public void SetContentImage(Sprite sprite)
+        {
+            _contentImage.sprite = sprite;
+        }
+
+        public void OnClick()
+        {
+            Clicked.Invoke(this);
+        }
+    }
+}
